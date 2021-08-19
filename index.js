d5e4623c8916ae059ad5605c33cad31992277149
truonghdpk <kyle@techainer.com>
2021-08-19 03:59:53 +0000

    update mock api

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-const { ApolloServer } = require("apollo-server");
-var { typeDefs } = require('./types');
+const { ApolloServer } = require('apollo-server')
+var { typeDefs } = require('./types')
 
 const mocks = {
-    String: () => 'Example Data',
-    DateTime: () => {
-      return new Date();
-    },
-    Date: () => {
-        return new Date();
+  String: () => 'Example Data',
+  DateTime: () => {
+    return new Date()
+  },
+  Date: () => {
+    return new Date()
+  },
+  SystemQuery: () => {
+    return {
+      info: {
+        setup: false,
+        __typename: 'SystemInfo',
       },
-    isSystem: () => true,
-    Upload: () => 'Upload',
-    Group: () => {
-      return {
-      id: "123",
-      name: "123",
-      isSystem: true,
-      permissions: ["manage:system"]
-      }
+      __typename: 'SystemQuery',
     }
-    
-};
-  
-const server = new ApolloServer({ typeDefs, mocks });
+  },
+  isSystem: () => true,
+  Upload: () => 'Upload',
+  Group: () => {
+    return {
+      id: '123',
+      name: '123',
+      isSystem: true,
+      permissions: ['manage:system'],
+    }
+  },
+}
 
-server.listen({port: 8888}).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+const server = new ApolloServer({ typeDefs, mocks })
+
+server.listen({ port: 8888 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+})
